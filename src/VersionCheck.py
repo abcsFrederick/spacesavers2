a823b1229128240fe48c02a5d5fbe938237eb2fd
kopardev <vishal.koparde@nih.gov>
2023-05-02 21:10:34 -0400

    adding __version__

--- src/VersionCheck.py
+++ src/VersionCheck.py
@@ -1,5 +1,8 @@
 import sys
 
+global __version__
+__version__ = 'v0.1'
+
 def version_check():
     # version check
     # glob.iglob requires 3.11 for using "include_hidden=True"
@@ -8,3 +11,4 @@ def version_check():
         assert sys.version_info >= MIN_PYTHON
     except AssertionError:
         exit(f"{sys.argv[0]} requires Python {'.'.join([str(n) for n in MIN_PYTHON])} or newer")
+
