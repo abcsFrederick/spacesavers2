ed8aeb50218b89a67c915b7e8884baff9d08b888
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-13 20:48:03 -0400

    feat: run this script from anywhere

--- bin/render_report_biowulf.sh
+++ bin/render_report_biowulf.sh
@@ -2,4 +2,4 @@
 module load singularity
 SINGULARITY_CACHEDIR=/data/CCBR_Pipeliner/SIFS
 echo "cd /mnt && Rscript bin/render.R" |\
-    singularity exec -C -B $PWD:/mnt,/data/CCBR_Pipeliner/userdata/spacesavers2/:/mnt/data docker://kellysovacool/spacesavers2:0.1.0 bash
+    singularity exec -C -B /data/CCBR_Pipeliner/spacesavers2-report:/mnt,/data/CCBR_Pipeliner/userdata/spacesavers2/:/mnt/data docker://kellysovacool/spacesavers2:0.1.0 bash
