cd17d119510e105f32ea46ebdc5ebc6b4f952761
kopardev <vishal.koparde@nih.gov>
2023-10-17 16:51:37 -0400

    feature: print version with  or

--- src/VersionCheck.py
+++ src/VersionCheck.py
@@ -19,3 +19,7 @@ def version_check():
         exit(
             f"{sys.argv[0]} requires Python {'.'.join([str(n) for n in MIN_PYTHON])} or newer"
         )
+
+
+def version_print():
+    exit(f"version: {__version__}")
