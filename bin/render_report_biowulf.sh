78d68ceac7d8a254296098750edf48f1fd8af9bd
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-17 20:15:21 -0400

    chore: render w/ updated docker

--- bin/render_report_biowulf.sh
+++ bin/render_report_biowulf.sh
@@ -2,4 +2,4 @@
 module load singularity
 SINGULARITY_CACHEDIR=/data/CCBR_Pipeliner/SIFS
 echo "cd /mnt && Rscript bin/render.R" |\
-    singularity exec -C -B /data/CCBR_Pipeliner/spacesavers2-report:/mnt,/data/CCBR_Pipeliner/userdata/spacesavers2/:/mnt/data docker://nciccbr/spacesavers2:0.1.0 bash
+    singularity exec -C -B /data/CCBR_Pipeliner/spacesavers2-report:/mnt,/data/CCBR_Pipeliner/userdata/spacesavers2/:/mnt/data docker://nciccbr/spacesavers2:0.1.1 bash
