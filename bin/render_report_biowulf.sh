911a139109ae9aacc72884082c9d45a4c2dd8ff8
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-24 19:55:01 -0400

    chore: update working path

--- bin/render_report_biowulf.sh
+++ bin/render_report_biowulf.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+# to be executed from /data/CCBR_Pipeliner/Tools/spacesavers2/report
+# Usage: bash bin/render_report_biowulf.sh
 module load singularity
 SINGULARITY_CACHEDIR=/data/CCBR_Pipeliner/SIFS
 echo "cd /mnt && Rscript bin/render.R" |\
-    singularity exec -C -B /data/CCBR_Pipeliner/bin/spacesavers2/report:/mnt,/data/CCBR_Pipeliner/userdata/spacesavers2/:/mnt/data docker://nciccbr/spacesavers2:0.1.1 bash
+    singularity exec -C -B $PWD:/mnt,/data/CCBR_Pipeliner/userdata/spacesavers2/:/mnt/data docker://nciccbr/spacesavers2:0.1.1 bash
