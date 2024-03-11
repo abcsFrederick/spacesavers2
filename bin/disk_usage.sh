8182088c19f9973f305b238daad8a0465f5a539b
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-03-11 11:11:05 -0400

    docs: redirect site to CCBR

--- bin/disk_usage.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-today=`date +"%Y-%m-%d %H:%M:%S"`
-
-df /data/CCBR |\
-  awk -v today="$today" 'NR==1{$(NF+1)="datetime"} NR>1{$(NF+1)=today}1' |\
-  sed -E 's/Mounted on/Mounted_on/' |\
-  sed -E 's/ +/\t/g' |\
-  tail -n 1 \
-  >> results/disk_usage.tsv
